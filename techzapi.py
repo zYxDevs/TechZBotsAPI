91958ede5a986d0f8888d336a723da4640d3126e
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-04-30 18:38:11 +0530

    Update techzapi.py

--- techzapi.py
+++ techzapi.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from fastapi import FastAPI
 from utils import search_unsplash, search_wall, telegraph, generate_logo, get_nyaa_info
 from fastapi.responses import RedirectResponse, FileResponse
@@ -12,30 +12,33 @@ async def read_root():
 
 @app.get("/wall")
 async def read_item(query: str):
+    "Get direct links of wallpapers"
     data = await search_wall(query)
 
     if str(type(data)) == "<class 'str'>":
         return {"success": "False", "error": f"{data}"}
 
-    return {"success": "True", "images": data}
+    return json.dumps({"success": "True", "images": data}, indent=4)
 
 @app.get("/unsplash")
 async def read_item(query: str):
+    "Get direct links of images from unsplash"
     data = await search_unsplash(query)
 
     if str(type(data)) == "<class 'str'>":
-        return {"success": "False", "error": f"{data}"}
+        return json.dumps({"success": "False", "error": f"{data}"}, indent=4)
 
-    return {"success": "True", "images": data}
+    return json.dumps({"success": "True", "images": data}, indent=4)
 
 @app.get("/logo")
 async def read_item(text: str):
+    "Generate a random logo"
     text = text.replace("%20"," ").upper().strip()
     data = await generate_logo(text)
 
     if "error" in str(data):
         error = data.replace("error",'').strip()
-        return {"success": "False", "error": f"{error}"}
+        return json.dumps({"success": "False", "error": f"{error}"}, indent=4)
 
     file_size = int(os.path.getsize(data))
 
@@ -43,7 +46,7 @@ async def read_item(text: str):
         telegraph_link = await telegraph(data,text)
         if "error" in str(data):
             error = data.replace("error",'').strip()
-            return {"success": "False", "error": f"{error}"}
+            return json.dumps({"success": "False", "error": f"{error}"}, indent=4)
         return RedirectResponse(telegraph_link)
 
     return FileResponse(data)
@@ -51,7 +54,6 @@ async def read_item(text: str):
 
 @app.get("/nyaa")
 async def get_nyaa(code: int):
-  "Get info from nyaa"
+  "Get info from nyaa using code"
   x = await get_nyaa_info(code)
-  code = x
-  return code
+  return json.dumps(x, indent=4)
