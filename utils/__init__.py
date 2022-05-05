c3dad633e8c77464cacc04308b2301257862830a
Anshul Garg <garganshul553@gmail.com>
2022-05-06 01:03:21 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,11 @@ from utils.wall import *
 from utils.extra import *
 from utils.logo import *
 from utils.nyaa import *
+
+
+import glob
+from os.path import basename, dirname, isfile
+
+mod_paths = glob.glob(dirname(__file__) + "/*.py")
+for f in mod_paths if isfile(f) and f.endswith(".py"):
+  from .basename(f)[:-3] import *
