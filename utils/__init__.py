eba532d6423208727e432ca0b9a7cd15481f5433
Anshul Garg <garganshul553@gmail.com>
2022-05-06 01:08:20 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,14 @@
+"""
 from utils.unsplash import *
 from utils.wall import *
 from utils.extra import *
 from utils.logo import *
 from utils.nyaa import *
-
-
-import glob
-from os.path import basename, dirname, isfile
+"""
 
 mod_paths = glob.glob(dirname(__file__) + "/*.py")
-for f in mod_paths if isfile(f) and f.endswith(".py"):
-  from f import *
+
+for f in mod_paths:
+  if isfile(f) and f.endswith(".py"):
+    pr = basename(f)[:-3]
+    from pr import *
