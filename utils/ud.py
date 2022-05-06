73b4bcd01ff81b655b3230a7b7aaf2157e392a04
Anshul Garg <garganshul553@gmail.com>
2022-05-06 13:00:13 +0530

    Update ud.py

--- utils/ud.py
+++ utils/ud.py
@@ -1,25 +1,27 @@
 import aiohttp
 
+
 async def ud(word):
-    
-     requests = aiohttp.ClientSession()
-     r = await requests.get(f"http://api.urbandictionary.com/v0/define?term={word}") 
-     
-     r = await r.json() 
+    requests = aiohttp.ClientSession()
+    r = await requests.get(f"http://api.urbandictionary.com/v0/define?term={word}")
+    r = await r.json()
+    await requests.close()
+    z = []
+    for x in r['list']:
+        z.append([x["thumbs_up"] - x["thumbs_down"]])
+    z.sort()
+    z.reverse()
+    fuck = []
+    for x in r['list']:
+        if (x["thumbs_up"] - x["thumbs_down"]) == z[0][0]:
+            fuck.append(x)
 
+    resp = []
+    for result in fuck:
+        re = {}
+        re['definition'] = result['definition']
+        re['example'] = result['example']
+        resp.append(re)
 
-     await requests.close()
+    return resp
 
-
-     z = [] 
-     for x in r['list']:
-         z.append([x["thumbs_up"] - x["thumbs_down"]])
-     z.sort()
-     z.reverse()
-     fuck = []
-     
-     for x in r['list']:
-       if (x["thumbs_up"] - x["thumbs_down"]) == z[0][0]:
-          fuck.append(x)
-     
-     return fuck[0]
