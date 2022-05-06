a4831b57407a9d267fd6966a8a3361fb08a2201d
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-05-06 22:18:03 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,6 @@
-"""
 from utils.unsplash import *
 from utils.wall import *
 from utils.extra import *
 from utils.logo import *
 from utils.nyaa import *
-"""
-import glob
-from os.path import basename, dirname, isfile 
-
-mod_paths = glob.glob(dirname(__file__) + "/*.py")
-
-for f in mod_paths:
-  if isfile(f) and f.endswith(".py"):
-    pr = basename(f)[:-3]
-    from pr import *
+from utils.ud import *
