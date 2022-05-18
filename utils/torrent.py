043ae289feadb980f278f2f014d116ecb5b865a8
Anshul Garg <garganshul553@gmail.com>
2022-05-18 21:16:12 +0530

    Update torrent.py

--- utils/torrent.py
+++ utils/torrent.py
@@ -67,15 +67,6 @@ async def yts(query):
     
         l = {"name": nam, "magnets": magnets}
         links.append(l)
-
-    for x in links:
-       d = []
-       for a in links:
-         if a == x:
-           d.append(links.index(a))
-         else:
-           pass
-         links.pop(d[0])   
     #closing aiohttp connection
 
     await requests.close()
