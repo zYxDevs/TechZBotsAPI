9f0eeadb148e6d5b82bf78b8d780989b357dd6bf
TechShreyash <shreyashdheemar123@gmail.com>
2022-07-15 15:07:59 +0530

    .

--- techzapi.py
+++ techzapi.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 from typing import Optional
+from utils.db import add_to_db
 from utils.gogo import gogo_scrapper
 from utils import thumbnail
 from utils.thumbnail import gen_thumb
@@ -12,6 +13,7 @@ app = FastAPI()
 
 @app.get("/")
 async def home():
+    await add_to_db(3)
     return {"status": "TechZBots - Api working fine..."}
 
 
@@ -19,9 +21,11 @@ async def home():
 async def get_wall(query: str):
     "Get direct links of wallpapers"
     data = await search_wall(query)
+    await add_to_db(2)
 
     if str(type(data)) == "<class 'str'>":
         return {"success": "False", "error": f"{data}"}
+
 
     return {"success": "True", "images": data}
 
@@ -29,6 +33,8 @@ async def get_wall(query: str):
 async def get_wall(query: str):
     "Get direct links of wallpapers in different qualities"
     data = await search_wallpro(query)
+
+    await add_to_db(2)
 
     if str(type(data)) == "<class 'str'>":
         return {"success": "False", "error": f"{data}"}
@@ -39,6 +45,7 @@ async def get_wall(query: str):
 async def get_unsplash(query: str):
     "Get direct links of images from unsplash"
     data = await search_unsplash(query)
+    await add_to_db(2)
 
     if str(type(data)) == "<class 'str'>":
         return {"success": "False", "error": f"{data}"}
@@ -51,6 +58,7 @@ async def make_logo(text: str, square: Optional[bool] = None):
     text = text.replace("%20"," ").replace("+"," ").replace("_"," ").upper().strip()
     
     data = await generate_logo(text,str(square))
+    await add_to_db(2)
 
     if "error" in str(data):
         error = data.replace("error",'').strip()
@@ -72,11 +80,13 @@ async def make_logo(text: str, square: Optional[bool] = None):
 async def get_nyaa(code):
     "Get info from nyaa using code"
     x = await get_nyaa_info(code=code)
+    await add_to_db(3)
     return x
 
 @app.get("/latest")
 async def nyaa_latest(max: Optional[int] = None):
     "Get Latest From Nyaa"
+    await add_to_db(3)
     if max:
         x = await get_nyaa_latest(max)
     else:
@@ -87,6 +97,7 @@ async def nyaa_latest(max: Optional[int] = None):
 async def get_ud(word: str, max: Optional[int] = None):
     "Get meaning of a word from urban dictionary"
     x = await get_urbandict(word)
+    await add_to_db(3)
     z = {}
     if len(x) > 0:
       z['success'] = 'True'
@@ -98,33 +109,37 @@ async def get_ud(word: str, max: Optional[int] = None):
     if max:
       x = x[:max]
     z['results'] = x
+    await add_to_db(3)
     return z
 
 @app.get("/torrent")
 async def torrent(query: str, max: Optional[int] = None):
-  "Get Torrent currently only yts supported"
-  x = await yts(query)
-  res = {}
-  if x: 
-    res['success'] = 'True'
-    res['query'] = query
-    res['results'] = x
-  else:
-    res['success'] = 'False'
-    res['error'] = 'Results Not Found'
-
-  return res
+    "Get Torrent currently only yts supported"
+    x = await yts(query)
+    await add_to_db(3)
+    res = {}
+    if x: 
+        res['success'] = 'True'
+        res['query'] = query
+        res['results'] = x
+    else:
+        res['success'] = 'False'
+        res['error'] = 'Results Not Found'
+    await add_to_db(3)
+    return res
 
 @app.get("/lyrics")
 async def search_lyrics(query: str):
     "Search lyrics"
     lyrics = await get_lyrics(query)
+    await add_to_db(3)
 
     data = {}
 
     data["success"] = 'True'
     data["query"] = query
     data["lyrics"] = lyrics
+    await add_to_db(3)
     return data
 
 @app.get("/thumb")
@@ -134,7 +149,7 @@ async def generate_thumbnail(videoid: str, botname: Optional[str] = None):
     if not botname:
       botname = "SiestaXMusic"
     thumb = await gen_thumb(videoid,botname)
-
+    await add_to_db(1)
     return RedirectResponse(thumb)
 
 @app.get("/gogo")
--- utils/db.py
+++ utils/db.py
@@ -1,10 +1,31 @@
-data = {}
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-def add_to_db(type):
+MONGO_DB_URI = "mongodb+srv://techz:wall@techzwallbotdb.katsq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client.api.req
+
+data = {'thumb':0,'wall/logo':0,'other':0}
+pos = 0
+
+async def add_to_db(type):
+    global pos, data
+
+    try:
+        if data['thumb'] == 0:
+            x = await db.find_one({"id": 1})
+            data = x['data']
+    except:
+        pass
 
     if type == 1:
         data['thumb'] += 1
-    elif type == 1:
-        data['thumb'] += 1
-    elif type == 1:
-        data['thumb'] += 1
+    elif type == 2:
+        data['wall/logo'] += 1
+    elif type == 3:
+        data['other'] += 1
+
+    if pos > 20 :
+        pos = 0
+        data = await db.update_one({"id": 1},{"$set": {"data": data}},upsert=True)
+    return
